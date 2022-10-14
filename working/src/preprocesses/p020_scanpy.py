a78ed9b229eb52f7970bf9f5f1ec7ec48d1e150f
Yoshio Sugiyama <nenegi.01mo@gmail.com>
2022-10-14 20:45:45 +0900

    feat: Raw data scanpy.
    Signed-off-by: Yoshio Sugiyama <nenegi.01mo@gmail.com>

--- working/src/preprocesses/p020_scanpy.py
+++ working/src/preprocesses/p020_scanpy.py
@@ -16,8 +16,8 @@ class CustomScanPy(BaseTransformer):
 
     def __init__(self, c):
         self.seed = c.global_params.seed
-        self.n_components = 0
-        self.columns = [f"scanpy_pca_{n}" for n in range(self.n_components)]
+        self.n_components = 240
+        self.columns = [f"scanpy_pca{self.n_components}_{n}" for n in range(self.n_components)]
 
     def fit(self, df):
         ...
@@ -32,20 +32,20 @@ class CustomScanPy(BaseTransformer):
 
         sc.pp.filter_genes(adata, min_cells=300)
 
-        adata.var["mt"] = adata.var_names.str.contains("_MT-")
-        adata.var["mt_a"] = adata.var_names.str.contains("_MT-A")
-        adata.var["mt_c"] = adata.var_names.str.contains("_MT-C")
-        adata.var["mt_n"] = adata.var_names.str.contains("_MT-N")
-        adata.var["mt_r"] = adata.var_names.str.contains("_MT-R")
-        adata.var["mt_t"] = adata.var_names.str.contains("_MT-T")
+        # adata.var["mt"] = adata.var_names.str.contains("_MT-")
+        # adata.var["mt_a"] = adata.var_names.str.contains("_MT-A")
+        # adata.var["mt_c"] = adata.var_names.str.contains("_MT-C")
+        # adata.var["mt_n"] = adata.var_names.str.contains("_MT-N")
+        # adata.var["mt_r"] = adata.var_names.str.contains("_MT-R")
+        # adata.var["mt_t"] = adata.var_names.str.contains("_MT-T")
 
-        sc.pp.calculate_qc_metrics(
-            adata,
-            qc_vars=["mt", "mt_a", "mt_c", "mt_n", "mt_r", "mt_t"],
-            percent_top=None,
-            log1p=False,
-            inplace=True,
-        )
+        # sc.pp.calculate_qc_metrics(
+        #     adata,
+        #     qc_vars=["mt", "mt_a", "mt_c", "mt_n", "mt_r", "mt_t"],
+        #     percent_top=None,
+        #     log1p=False,
+        #     inplace=True,
+        # )
 
         sc.pp.normalize_total(adata, target_sum=1e4)
         sc.pp.log1p(adata)
@@ -55,27 +55,28 @@ class CustomScanPy(BaseTransformer):
 
         # sc.pp.scale(train_cite_inputs_adata, max_value=10)
 
-        # sc.pp.pca(adata, n_comps=self.n_components, random_state=self.seed)
-        # pca_df = pd.DataFrame(adata.obsm["X_pca"], index=adata.obs_names, columns=self.columns)
+        sc.pp.pca(adata, n_comps=self.n_components, random_state=self.seed)
+        pca_df = pd.DataFrame(adata.obsm["X_pca"], index=adata.obs_names, columns=self.columns)
 
-        mt_cols = [
-            "total_counts_mt",
-            "total_counts_mt_a",
-            "total_counts_mt_c",
-            "total_counts_mt_n",
-            "total_counts_mt_r",
-            "total_counts_mt_t",
-        ]
-        mt_df = adata.obs[mt_cols]
+        # mt_cols = [
+        #     "total_counts_mt",
+        #     "total_counts_mt_a",
+        #     "total_counts_mt_c",
+        #     "total_counts_mt_n",
+        #     "total_counts_mt_r",
+        #     "total_counts_mt_t",
+        # ]
+        # mt_df = adata.obs[mt_cols]
 
-        dt = DistTransformer()
-        dt.fit(mt_df)
-        mt_df = dt.transform(mt_df)
+        # dt = DistTransformer()
+        # dt.fit(mt_df)
+        # mt_df = dt.transform(mt_df)
 
         # adata_df = pd.DataFrame(adata.X, index=adata.obs_names, columns=adata.var_names)
 
         # df = pd.concat([adata_df, mt_df], axis=1)
         # df = pd.concat([pca_df, mt_df], axis=1)
-        df = mt_df
+        # df = mt_df
+        df = pca_df
 
         return df
